fe34b4b6c7540a07cd0597a7a6d141d4fc6ee4f8
yiming.wang <yiming.wang@xbotspace.com>
2024-07-23 14:37:29 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gin-stream-middleware
-the middleware for gin stream
+`gin` 框架的流式中间件
+
+- 对于常规的中间件，只能在接口的函数处理完成以后，在 `c.Next()` 后续中一次性处理。因此，对于流式的返回，这样的中间件可能并不满足你的需求。
+- 本库提供了流式的中间件，其实现方式是重写了将返回内容写入 buffer 的 write 函数。这样，对于流式每次写入返回内容时，都可以触发自定义的处理函数，以充当中间件的功能。
+- 要使用本库，你需要重写 `CallbackInstance` 结构体的 `Call` 方法，以实现自定义功能。
+- 具体使用实例请参见 `example.go`。
+
+# gin-stream-middleware
+Stream middleware for the `gin` framework
+
+- For regular middleware, it can only be processed once after the interface function is completed, in the subsequent `c.Next()`. Therefore, such middleware may not meet your requirements for stream returns.
+- This library provides stream middleware, which is implemented by rewriting the write function that writes the return content into the buffer. In this way, each time the stream writes return content, it can trigger a custom processing function to act as middleware.
+- To use this library, you need to rewrite the `Call` method of the `CallbackInstance` struct to implement custom functionality.
+- For specific usage examples, please see `example.go`.
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+	"io"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type Callback interface {
+	Call([]byte) ([]byte, error)
+	GetName() string
+}
+
+
+type StreamWriter struct {
+	gin.ResponseWriter
+	Buffer  *bytes.Buffer
+	Context *gin.Context
+	Callback []Callback
+	Flusher http.Flusher
+}
+
+func (r StreamWriter) Write(b []byte) (int, error) {
+	resp := b
+	var err error
+	for _, callback := range r.Callback {
+		resp, err = callback.Call(resp)
+		if err != nil {
+			return 0, fmt.Errorf("error occurred in '%s' stream middleware， reason: %v", callback.GetName(), err)
+		}
+	}
+	written, err := r.ResponseWriter.Write(resp)
+	r.Flush()
+	return written, err
+}
+
+func (r *StreamWriter) Flush() {
+	r.Flusher.Flush()
+}
+
+
+func Register(callbacks []Callback) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		buffer := new(bytes.Buffer)
+		streamWriter := &StreamWriter{ResponseWriter: c.Writer, Buffer: buffer, Context: c, Callback: callbacks, Flusher: c.Writer.(http.Flusher)}
+		c.Writer = streamWriter
+		c.Next()
+	}
+}
+
+type CallbackInstance struct {
+	Callback
+	Name string
+}
+
+func (c CallbackInstance) Call(b []byte)([]byte, error) {
+	fmt.Printf("call >>>>>, message: %s", string(b))
+	return b, nil
+}
+
+func (c CallbackInstance) GetName() string {
+	return c.Name
+}
